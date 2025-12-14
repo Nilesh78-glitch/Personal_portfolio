053a4fe5e808ca5a19db881fd270bb0de945bdd1
Nilesh78-glitch <nilesh58rai@gmail.com>
2025-12-14 11:16:03 +0530

    change in name

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
           </h1>
 
           <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
-            Animesh Kumar
+            Nilesh Singh
           </h2>
 
           <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
